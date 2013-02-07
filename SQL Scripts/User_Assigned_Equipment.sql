9eea09ed742b7d3a04e3a23805ca530f65d720ed
Shawn Anderson <shawnboy@gmail.com>
2013-02-07 13:41:17 -0700

    Fixed User_Assigned_Equipment SQL Script
    Output display modifications

--- SQL Scripts/User_Assigned_Equipment.sql
+++ SQL Scripts/User_Assigned_Equipment.sql
@@ -1,7 +1,7 @@
-select Users.UserUVID as 'User UVID', Users.UserFName + Users.UserLName as 'User''s Name', Equipment.UVUInvID as 'UVU Inventory ID', Model.ModelName as 'Model', Type.TypeID as 'Type', Equipment.SerialNum as 'Computer Serial #'
+select Users.UserUVID as 'User UVID', Users.UserFName + ' ' + Users.UserLName as 'User''s Name', Equipment.UVUInvID as 'UVU Inventory ID', Mfg.MfgName + ' ' + Model.ModelName as 'Model', Type.TypeName as 'Type', Equipment.SerialNum as 'Computer Serial #'
 from Equipment
 Inner Join Model on Equipment.ModelID = Model.ModelID
 Inner Join Type on Equipment.TypeID = Type.TypeID
 Inner Join Users on Equipment.UserUVID = Users.UserUVID
+Inner Join Mfg on Mfg.MfgID = Model.MfgID
 order by 'User UVID'
-
